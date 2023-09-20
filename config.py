79d7561e88c6fb2468ff638908e99f28699f8927
gonen raveh <gonen.raveh@gmail.com>
2023-09-20 18:35:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ mvtec_path = "/mnt/data-server-storage/okatani_lab/mvtec/"
 # "cait_m48_448" or "deit_base_distilled_patch16_384" is supported.
 backbone = "cait_m48_448"
 device = "cuda:0"
-batch_size = 32
+batch_size = 16  # original=32
 nb_epoch = 2
 learning_rate = 2e-4
 warmup_epoch = 1
